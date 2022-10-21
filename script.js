988ea1e17a99770b42a74dbd0d7bad44b7e5918d
Aymane <aymanebk@gmail.com>
2022-10-21 17:07:58 +0100

    Add more visual elements

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function getComputerMove(){
     let move = Math.floor(Math.random()*3);
     if (move==0) {
+        cpuimage.src = "./images/01.svg"
         return 'Rock';
     }else if (move == 1) {
+        cpuimage.src = "./images/02.svg"
         return 'Paper'; 
     }else {
+        cpuimage.src = "./images/03.svg"
         return 'Scissors'
     }
 }
@@ -32,39 +35,51 @@ function playRound(playerMove, computerMove){
 
 
  
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('input');
 let pmove
 let winner
+let pimage = document.querySelector('.playerchoice')
+let cpuimage = document.querySelector('.cpuchoice')
 let cpuScore = 0
 let pScore = 0
 const display = document.querySelector('.display-result');
-const text = document.createElement('p');
-const score = document.createElement('p');
+const text = document.createElement('h1');
+const score = document.createElement('h4');
 
-score.textContent = "SCORE  Player : 0    Computer : 0    ";
-
+score.innerHTML = "SCORE<br/><br/>  Player : 0    Computer : 0    ";
+text.textContent = "Let's GO !!"
 display.appendChild(text);
 display.appendChild(score);
 
-
-const res = buttons.forEach((button) => {
+buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
         if (pScore == 5 || cpuScore == 5){
-            text.textContent = "The game has ended, piss off !!!!!!!"
+            text.textContent = "I said bye !! if you wanna play again click the play again BUTTON"
             return
         }else{
-            pmove = button.textContent;
+            pmove = button.className;
+            if (pmove=="Rock"){
+                pimage.src = "./images/01.svg"
+            }else if(pmove =="Paper"){
+                pimage.src = "./images/02.svg"
+            }else{
+                pimage.src= "./images/03.svg"
+            }
             winner = playRound(pmove, getComputerMove());
             (winner == 'Draw') ? text.textContent = "Draw" : (text.textContent = winner + " Wins !!") ;
             if (winner == "Player"){
-                pScore += 1
+                pScore++
             }else if(winner == "Computer"){
-                cpuScore += 1
+                cpuScore++
             }
-            score.textContent = `Score Player : ${pScore}    Computer : ${cpuScore}    `;
+            score.innerHTML = `SCORE<br/> <br/> Player: ${pScore}    Computer : ${cpuScore}    `;
+    }
+        if (pScore == 5 || cpuScore == 5){
+            text.textContent = "The game has ended, piss off !!!!!!!"
     }
     });
+
 });
 
 
